598b8c19b8d96c483b768e6ffd4c0bb25630bc3e
11907223 <e.vaniterson98@gmail.com>
2023-06-05 16:55:32 +0200

    created classes and data structure

---
+++ code/classroom.py
@@ -0,0 +1,11 @@
+""" Classroom """
+
+
+class Classroom:
+    def __init__(self):
+        # TODO
+        # id,naam, capaciteit
+        pass
+
+    def add_lecture(self, lecture: object, timeslot: str):
+        pass
---
+++ code/student.py
@@ -0,0 +1,11 @@
+""" Students """
+
+
+class Student:
+    def __init__(self):
+        # TODO
+        # id, achternaam, voornaam, studentnummer, vakken
+        pass
+
+    def add_lecture(self, lecture: object, timeslot: str, classroom: object):
+        pass
