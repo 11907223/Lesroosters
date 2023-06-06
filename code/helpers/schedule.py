fb63c6da1213c163394ef99905118f4fe6335a2f
Niels <niels.huang@student.uva.nl>
2023-06-06 17:04:10 +0200

    functional json structure schedule

--- code/helpers/schedule.py
+++ code/helpers/schedule.py
@@ -1,9 +1,32 @@
-import pandas as pd
+import copy
 
 
 def scheduler():
     weekday = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
-    timeslot = [f"{i}:00 - {i + 2}:00" for i in range(9, 19, 2)]
-    df = pd.DataFrame(index=timeslot, columns=weekday)
-    building_schedule = {i: df for i in range(7)}
-    return building_schedule
+    timeslot = {i: {} for i in range(9, 19, 2)}
+    schedule = {}
+    for day in weekday:
+        schedule[day] = copy.deepcopy(timeslot)
+    return schedule
+
+
+class Schedule:
+    def __init__(self) -> None:
+        self.schedule: dict[any] = scheduler()
+
+    def add_course(self, day: str, timeslot: int, course: str, type: str, location: str):
+        """
+        
+        Args:
+            day (str): Day.
+            timeslot (str): sada
+            course (str):
+
+        """
+        self.schedule[day][timeslot].update({location: {"coursename": course, "type": type}})
+
+s = Schedule()
+s.add_course("Monday", 9, "Statistics", "werkcollege", "hier")
+s.add_course("Friday", 11, "Ananas", "diner", "daar")
+s.add_course("Monday", 9, "Patat", "ontbijt", "daar")
+print(s.schedule)
