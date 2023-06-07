7a360fe398e8df6912bad4822541f6f45910fec3
Niels <niels.huang@student.uva.nl>
2023-06-07 12:27:33 +0200

    prototype random scheduler functional but ugly

--- code/helpers/schedule.py
+++ code/helpers/schedule.py
@@ -1,32 +1,34 @@
 import copy
+from collections import defaultdict
 
 
 def scheduler():
     weekday = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
-    timeslot = {i: {} for i in range(9, 19, 2)}
-    schedule = {}
-    for day in weekday:
-        schedule[day] = copy.deepcopy(timeslot)
+    hall_ids = ["A1.04", "A1.06", "A1.08", "A1.10", "B0.201", "C0.110", "C1.112"]
+    hallslot = {id: {} for id in hall_ids}
+    timeslot = {str(i): copy.deepcopy(hallslot) for i in range(9, 19, 2)}
+    schedule = {day: copy.deepcopy(timeslot) for day in weekday}
     return schedule
 
 
 class Schedule:
     def __init__(self) -> None:
-        self.schedule: dict[any] = scheduler()
+        self.schedule = scheduler()
 
-    def add_course(self, day: str, timeslot: int, course: str, type: str, location: str):
+    def find_next_empty_slot(self):
+        pass
+
+    def add_course(
+        self, day: str, timeslot: int, course: str, type: str, location: str
+    ):
         """
-        
+
         Args:
             day (str): Day.
             timeslot (str): sada
             course (str):
 
         """
-        self.schedule[day][timeslot].update({location: {"coursename": course, "type": type}})
-
-s = Schedule()
-s.add_course("Monday", 9, "Statistics", "werkcollege", "hier")
-s.add_course("Friday", 11, "Ananas", "diner", "daar")
-s.add_course("Monday", 9, "Patat", "ontbijt", "daar")
-print(s.schedule)
+        self.schedule[day][timeslot].update(
+            {location: {"coursename": course, "type": type}}
+        )
