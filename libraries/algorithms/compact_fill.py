922a5694721491baa87ddda6a5d402787970df8d
Niels <niels.huang@student.uva.nl>
2023-06-13 14:18:46 +0200

    added compact fill class

--- libraries/algorithms/compact_fill.py
+++ libraries/algorithms/compact_fill.py
@@ -13,3 +13,17 @@ def fill_schedule(courses, schedule):
         students = courses[activity.course].students.values()
         for student in students:
             activity.add_student({student.index: student})
+
+
+class compact_fill:
+    def __init__(self) -> None:
+        # TODO convert to model (dict, acts(str))
+        # add act dict with students
+        pass
+
+    def fill(self, schedule, courses, students):
+        all_activities = []
+        for course_name in courses:
+            course = courses[course_name]
+            for activity in course.activities():
+                all_activities.append(activity)
