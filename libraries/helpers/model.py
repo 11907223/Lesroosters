65524fd013b9d90529368fc595983252c8d31322
Niels <niels.huang@student.uva.nl>
2023-06-13 17:13:02 +0200

    rebasing v2

--- libraries/helpers/model.py
+++ libraries/helpers/model.py
@@ -22,16 +22,13 @@ class Model:
     def translate_schedule_to_model(self):
         schedule_model = {}
         for index, entry in enumerate(self.schedule.as_list_of_dicts()):
-            activity = entry["activity"]
-            course = entry["course"]
-            schedule_model[index] = {"activity": activity, "course": course}
-        print(schedule_model)
-
+            activity = entry['activity']
+            course = entry['course']
+            schedule_model[index] = {'activity': activity, 'course': course}
+        
         students_per_activity = {}
-
-        for course in self.courses.values():
-            for activity in course.activities():
-                students_per_activity[
-                    f"{activity.course.name} {activity.category}"
-                ] = activity.students
+        for day in self.schedule.days.values():
+            for slot in day.slots:
+                if slot.activity is not None:
+                    students_per_activity[f"{slot.activity.course.name} {slot.activity.category}"] = slot.activity.students
         print(students_per_activity)
