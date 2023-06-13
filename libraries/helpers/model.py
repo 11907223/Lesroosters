b23c934b33ea04e0ac168fe87f421ebefee15ea8
Niels <niels.huang@student.uva.nl>
2023-06-13 16:52:00 +0200

    rebasing

--- libraries/helpers/model.py
+++ libraries/helpers/model.py
@@ -2,11 +2,10 @@ from libraries.classes.schedule import Schedule
 from libraries.helpers.load_data import load_courses, load_students
 from libraries.algorithms.random import Random
 
-
 class Model:
     def __init__(self) -> None:
-        self.schedule = random_schedule()
         self.courses = load_courses()
+        self.schedule = Random(Schedule(), self.courses).run()
         self.students = load_students(self.courses)
 
     def add_activity_to_schedule(self, activity: str):
@@ -16,8 +15,9 @@ class Model:
         pass
 
     def call_algorithm(self, algorithm):
-        if algorithm == "random":
-            random_schedule(self.courses, self.schedule)
+        pass
+        # if algorithm == "random":
+            # random_schedule(self.courses, self.schedule)
 
     def translate_schedule_to_model(self):
         schedule_model = {}
--- main.py
+++ main.py
@@ -11,20 +11,20 @@ if __name__ == "__main__":
     students = load_students(courses)
     schedule = Schedule()
 
-    # model = Model()
-    # model.translate_schedule_to_model()
+    model = Model()
+    print(model.translate_schedule_to_model())
+    schedule = random_schedule()
+    # random_schedule = Random(schedule, courses)
+    # random_schedule = random_schedule.run()
 
-    random_schedule = Random(schedule, courses)
-    random_schedule = random_schedule.run()
+    print(schedule.as_list_of_dicts())
 
-    # print(schedule.as_list_of_dicts())
-    # print(schedule)
-    # # other examples
-    # df = pd.DataFrame(schedule.as_list_of_dicts())
-    # print(
-    #     "THIS IS A DATAFRAME OF THE WHOLE SCHEDULE WHEN ACCOUNTING FOR ROOM SIZE: \n",
-    #     df,
-    # )
+    # other examples
+    df = pd.DataFrame(schedule.as_list_of_dicts())
+    print(
+        "THIS IS A DATAFRAME OF THE WHOLE SCHEDULE WHEN ACCOUNTING FOR ROOM SIZE: \n",
+        df,
+    )
 
     # room_scheme = df[df.room == "A1.04"]
     # print("THESE ARE ALL ACTIVITIES IN A1.04 ACROSS THE WHOLE WEEK: \n", room_scheme)
