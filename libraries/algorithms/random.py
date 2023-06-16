578cc1d926d708e977c135b6b18cce3064c3e885
Nina van der Meulen <ninaajwzz@gmail.com>
2023-06-16 16:46:32 +0200

    random now works with a model

--- libraries/algorithms/random.py
+++ libraries/algorithms/random.py
@@ -1,41 +1,18 @@
-from libraries.classes.schedule import Schedule
-from libraries.classes.course import Course
-from libraries.helpers.load_data import load_courses
-import random
+from random import randrange
 import copy
 
 class Random:
-    def __init__(self, empty_schedule, courses):
-        self.schedule = copy.deepcopy(empty_schedule)
-        self.activities = self.load_activities(courses)
+    def __init__(self, empty_model):
+        self.model = copy.deepcopy(empty_model)
 
-    def load_activities(self, courses):
-        # put all activities in a list & add students to activities
-        all_activities = []
-
-        for course_name in courses:
-            course = courses[course_name]
-            student_dict = course.students
-
-            for act in course.activities():
-                all_activities.append(act)
-
-                # add students to activity
-                act.add_students(student_dict)
-
-                # add activity to students
-                for student in student_dict.values():
-                    student.add_activity(act)
-        return all_activities
+        # add students to activities
+        for activity_tuple in self.model.participants:
+            for student in self.model.students:
+                self.model.add_student(student, activity_tuple)
     
     def run(self):
-        weekdays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
 
-        for activity in self.activities:
-            inserted_all_courses = False
-            # insert activity at random point
-            while not inserted_all_courses:
-                index = random.random() * (len(self.schedule.days[weekdays[1]].slots))
-                weekday = random.choice(weekdays)
-                inserted_all_courses = self.schedule.insert_activity(weekday, int(index), activity)
-        return self.schedule
+        for activity_tuple in self.model.participants:
+            random_slot = randrange(144)
+            self.model.add_activity(random_slot, activity_tuple)
+        return self.model
