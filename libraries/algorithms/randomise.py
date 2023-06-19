fd782689c49fc4f413edfe89399d2aec3ec4d0b8
Nina van der Meulen <ninaajwzz@gmail.com>
2023-06-19 16:54:48 +0200

    slot -> index

--- libraries/algorithms/randomise.py
+++ libraries/algorithms/randomise.py
@@ -7,21 +7,25 @@ class Random:
     Random generates a random schedule by randomly assigning activities to slots. 
     """
     def __init__(self, empty_model: Model):
-
-        # copy empty model object
         self.model = empty_model.copy()
     
-    def insert_randomly(self, activity_tuple):
-            # while loop ensures activity is added
-            while True:
-                random_slot = randrange(145)
+    def insert_randomly(self, activity_tuple) -> None:
+        """
+        Insert activity in random slot.
+        """
 
-                # addition successful
-                if self.model.add_activity(random_slot, activity_tuple) is True:
-                    break
-            return None
+        # while loop ensures activity is added
+        while True:
+            random_slot = randrange(145)
+            if self.model.add_activity(random_slot, activity_tuple) is True:
+                break
+
+        return None
     
     def run(self) -> Model: 
+        """
+        Generate a random solution.
+        """
         # randomly assign activities to slots
         for activity_tuple in self.model.participants:
             self.insert_randomly(activity_tuple)
@@ -31,7 +35,7 @@ class Random:
 
 def random_algorithm(iterations, empty_model: Model) -> Model:
     """
-    Generates a random schedule x times and returns the best one.
+    Generates a random solution x times and return the best one.
     """
     lowest_penalty = 1000
     for _ in range(iterations):
@@ -42,7 +46,7 @@ def random_algorithm(iterations, empty_model: Model) -> Model:
         # save best schedule 
         penalty_points = r.total_penalty()
         if penalty_points < lowest_penalty:
-            best_schedule = deepcopy(r)
+            best_schedule = r.copy()
             lowest_penalty = penalty_points
 
     return best_schedule
