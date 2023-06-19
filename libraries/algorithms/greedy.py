16acf913d5b29a8dc69a42633c26bcdc4053084e
11907223 <e.vaniterson98@gmail.com>
2023-06-19 17:01:19 +0200

    removed error from greedy

--- libraries/algorithms/greedy.py
+++ libraries/algorithms/greedy.py
@@ -47,14 +47,13 @@ class Greedy:
         return optimal_index, lowest_penalty
 
     def run(self) -> Model:
-
-    def run(self) -> Model:
         # loop over activities
         previous_penalty = 0
         for activity_tuple in self.activity_tuples:
-
             # find optimal index
-            optimal_index, lowest_penalty = self.get_optimal_index(activity_tuple, previous_penalty)
+            optimal_index, lowest_penalty = self.get_optimal_index(
+                activity_tuple, previous_penalty
+            )
 
             # add activity to optimal index
             self.model.add_activity(optimal_index, activity_tuple)
@@ -65,5 +64,5 @@ class Greedy:
         return self.model
 
     # VOOR RANDOM GREEDY
-        # randomly shuffle activities
-        # or make heuristic choice x % of the time, random choice x % of the time
+    # randomly shuffle activities
+    # or make heuristic choice x % of the time, random choice x % of the time
