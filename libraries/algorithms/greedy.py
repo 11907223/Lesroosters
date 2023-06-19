dde2b499018a48b1b274a26933adc8789792a769
Nina van der Meulen <ninaajwzz@gmail.com>
2023-06-19 15:04:57 +0200

    get_optimal_index added

--- libraries/algorithms/greedy.py
+++ libraries/algorithms/greedy.py
@@ -10,41 +10,47 @@ class Greedy:
 
     def __init__(self, empty_model: Model) -> None:
         self.solution = deepcopy(empty_model.copy())
-        # self.solution.add_all_students()
         self.activity_tuples = list(self.solution.participants.keys())
         # maybe randomly shuffle activities here
 
-    def run(self) -> Model:
-        # insert first activity randomly
-        self.solution.add_activity(random.randrange(145), self.activity_tuples[0])
-        self.activity_tuples.pop(0)
-
-        # loop over activities
-        for activity_tuple in self.activity_tuples:
+    # maybe slots arg, that carries the empty slots, so it doesnt loop over unused slots
+    def get_optimal_index(self, activity_tuple, previous_penalty):
             # initial value
             lowest_points = 1000
-
+            
             # loop over timeslots
-            # miss als slot gevuld is, hem weghalen uit de lijst als dat werkt
             for slot in self.solution.solution:
                 added = self.solution.add_activity(slot, activity_tuple)
                 if added is True:
-                    # miss als de penalty hetzelfde blijft, dan gewoon toevoegen.
-                    # PSEUDOCODE
-                    # schedule not full enough yet
-                    if self.solution.total_penalty() <= 3:
+
+                    # if penalty unchanged, optimal index is found
+                    if self.solution.total_penalty() == previous_penalty:
                         optimal_index = slot
                         self.solution.remove_activity(index=slot)
                         break
 
-                    # if penalty points lower than best, save index en best points
+                    # if penalty lower than best, save index en best points
                     elif self.solution.total_penalty() < lowest_points:
                         lowest_points = self.solution.total_penalty()
                         optimal_index = slot
 
                     self.solution.remove_activity(index=slot)
+                    
+            return optimal_index
+    
+    def run(self) -> Model:
+
+        # loop over activities
+        previous_penalty = 0
+        for activity_tuple in self.activity_tuples:
+
+            # find optimal slot
+            optimal_index = self.get_optimal_index(activity_tuple, previous_penalty)
 
             # add activity to optimal slot
             self.solution.add_activity(optimal_index, activity_tuple)
+
+            # update current penalty
+            previous_penalty = self.solution.total_penalty()
 
         return self.solution
