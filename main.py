c31b00c45aca7eee864f10752b0c780c136af830
11907223 <e.vaniterson98@gmail.com>
2023-06-20 09:31:20 +0200

    working on beam search

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import libraries.helpers.load_data as ld
 from libraries.algorithms.randomise import Random, random_algorithm
 from libraries.classes.model import Model
 from libraries.algorithms.greedy import Greedy
+from libraries.algorithms.beam_search import BeamSearch
 from libraries.algorithms.hillclimber import HillClimber
 from libraries.algorithms.simulated_annealing import SimulatedAnnealing
 
@@ -11,46 +12,51 @@ if __name__ == "__main__":
     halls = ld.load_halls()
 
     empty_model = Model()
-    random_model = Random(empty_model).run()
+    # random_model = Random(empty_model).run()
     # print(s.get_highest_students(3))
 
     # start_time = time.time()
 
-    print("empty index: ", random_model.get_random_index())
+    # print("empty index: ", random_model.get_random_index())
 
     # _________________________RANDOM ALGORITHM_______________________
-    iterations = 1
-    random_solution = random_algorithm(iterations, empty_model)
+    # iterations = 1
+    # random_solution = random_algorithm(iterations, empty_model)
 
-    print(
-        "THE BEST SCHEDULE FOUND WHEN USING RANDOM:\n",
-        random_solution.solution,
-        "\nPOINTS: ",
-        random_solution.total_penalty(),
-    )
-    # ________________________________________________________________
+    # print(
+    #     "THE BEST SCHEDULE FOUND WHEN USING RANDOM:\n",
+    #     random_solution.solution,
+    #     "\nPOINTS: ",
+    #     random_solution.total_penalty(),
+    # )
 
-    # ______________________HILLCLIMBER ALGORITHM_____________________
-    hillclimber = HillClimber(random_solution)
-    hillclimber.run(iterations=2000, verbose=True)
+    # ________________________BEAM SEARCH ALGORITHM________________________
 
-    print(
-        "THE BEST SCHEDULE FOUND WHEN USING HILLCLIMBER:\n",
-        hillclimber.schedule.solution,
-        "\nPOINTS: ",
-        hillclimber.schedule.total_penalty(),
-    )
+    beam_search = BeamSearch(empty_model)
+    beam_search = beam_search.run(2)
+    print(beam_search)
 
-    # ______________________SIMULATED ANNEALING_____________________
-    simulated_annealing = SimulatedAnnealing(random_solution)
-    simulated_annealing.run(iterations=2000, verbose=True)
+    # # ______________________HILLCLIMBER ALGORITHM_____________________
+    # hillclimber = HillClimber(random_solution)
+    # hillclimber.run(iterations=2000, verbose=True)
 
-    print(
-        "THE BEST SCHEDULE FOUND WHEN USING SIMULATED ANNEALING:\n",
-        simulated_annealing.schedule.solution,
-        "\n POINTS: ",
-        simulated_annealing.schedule.total_penalty(),
-    )
+    # print(
+    #     "THE BEST SCHEDULE FOUND WHEN USING HILLCLIMBER:\n",
+    #     hillclimber.schedule.solution,
+    #     "\nPOINTS: ",
+    #     hillclimber.schedule.total_penalty(),
+    # )
+
+    # # ______________________SIMULATED ANNEALING_____________________
+    # simulated_annealing = SimulatedAnnealing(random_solution)
+    # simulated_annealing.run(iterations=2000, verbose=True)
+
+    # print(
+    #     "THE BEST SCHEDULE FOUND WHEN USING SIMULATED ANNEALING:\n",
+    #     simulated_annealing.schedule.solution,
+    #     "\n POINTS: ",
+    #     simulated_annealing.schedule.total_penalty(),
+    # )
 
     # ________________________GREEDY ALGORITHM________________________
     # greedy_solution = Greedy(empty_model).run()
