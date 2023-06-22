6a6389b7d40fb2f62f904e3acbaa0d6ac1382aa9
11907223 <e.vaniterson98@gmail.com>
2023-06-22 12:42:33 +0200

    wotking on beam search

--- main.py
+++ main.py
@@ -17,45 +17,47 @@ if __name__ == "__main__":
 
     # start_time = time.time()
 
-    # _________________________RANDOM ALGORITHM________________________________________
-    random_algorithm = Random(empty_model)
-    random_algorithm.run()
-
-    print(
-        "THE BEST SCHEDULE FOUND WHEN USING RANDOM:\n",
-        # random_algorithm.model.solution,
-        "\nTOTAL POINTS: ",
-        random_algorithm.model.total_penalty(),
-        "\n evening points",
-        random_algorithm.model.evening_penalty(),
-        "\n student penalties:",
-        random_algorithm.model.student_schedule_penalties(),
-        "\n capacity penalty",
-        random_algorithm.model.total_capacity_penalties(),
-    )
-
-    # ________________________BEAM SEARCH ALGORITHM____________________________________
-
-    # beam_search = BeamSearch(empty_model)
-    # print("STARTING BEAM SEARCH ALGORITHM \n")
-    # start_time = time.time()
-    # beam_search.run(beam=5, runs=100, heuristic="capacity", verbose=True)
-    # end_time = time.time()
+    # # _________________________RANDOM ALGORITHM_______________________
+    # random_algorithm = Random(empty_model)
+    # random_algorithm.run()
 
     # print(
-    #     "THE BEST SCHEDULE FOUND WHEN USING BEAMSEARCH:\n",
-    #     # beam_search.model.solution,
+    #     "THE BEST SCHEDULE FOUND WHEN USING RANDOM:\n",
+    #     # random_algorithm.model.solution,
     #     "\nTOTAL POINTS: ",
-    #     beam_search.model.total_penalty(),
+    #     random_algorithm.model.total_penalty(),
     #     "\n evening points",
-    #     beam_search.model.evening_penalty(),
-    #     "\n conflict points:",
-    #     beam_search.model.conflict_penalty(),
+    #     random_algorithm.model.evening_penalty(),
+    #     "\n student penalties:",
+    #     random_algorithm.model.student_schedule_penalties(),
     #     "\n capacity penalty",
-    #     beam_search.model.total_capacity_penalties(),
-    #     "\n run time: ",
-    #     end_time - start_time,
+    #     random_algorithm.model.total_capacity_penalties(),
     # )
+
+    # print(random_algorithm.model.student_penalties)
+
+    # ________________________BEAM SEARCH ALGORITHM________________________
+
+    beam_search = BeamSearch(empty_model)
+    print("STARTING BEAM SEARCH ALGORITHM \n")
+    start_time = time.time()
+    beam_search.run(beam=5, runs=100, heuristic="capacity", verbose=True)
+    end_time = time.time()
+
+    print(
+        "THE BEST SCHEDULE FOUND WHEN USING BEAMSEARCH:\n",
+        # beam_search.model.solution,
+        "\nTOTAL POINTS: ",
+        beam_search.model.total_penalty(),
+        "\n evening points",
+        beam_search.model.evening_penalty(),
+        "\n conflict points:",
+        beam_search.model.conflict_penalty(),
+        "\n capacity penalty",
+        beam_search.model.total_capacity_penalties(),
+        "\n run time: ",
+        end_time - start_time,
+    )
 
     # ______________________HILLCLIMBER ALGORITHM______________________________________
     hillclimber = HillClimber(random_algorithm.model)
