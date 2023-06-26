b46abaa8d940f408a3820101231a8768c69425af
Nina van der Meulen <ninaajwzz@gmail.com>
2023-06-26 11:15:58 +0200

    upper bug fixed

--- libraries/helpers/print_results.py
+++ libraries/helpers/print_results.py
@@ -5,7 +5,7 @@ def print_results(algorithm: str, model:Model, runtime):
     Prints results of a model generated with an algorithm.
     """
     
-    print(f'THE BEST SCHEDULE FOUND WHEN USING {algorithm.upper}:\n', model.solution,
+    print(f'THE BEST SCHEDULE FOUND WHEN USING {algorithm.upper()}:\n', model.solution,
         '\n POINTS: ', model.total_penalty(),
         '\n evening points:', model.evening_penalty(),
         '\n conflict points:', model.student_schedule_penalties(),
