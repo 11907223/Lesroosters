5ded89aadc689fd2706d21b8c2916e836c515fce
Nina van der Meulen <ninaajwzz@gmail.com>
2023-06-26 13:15:30 +0200

    updated

--- libraries/helpers/print_results.py
+++ libraries/helpers/print_results.py
@@ -1,22 +1,20 @@
 from libraries.classes.model import Model
 
-
-def print_results(algorithm: object, runtime):
+def print_results(algorithm_name : str, model : Model, runtime):
     """
     Prints results of a model generated with an algorithm.
     """
-
     print(
-        f"THE BEST SCHEDULE FOUND WHEN USING {algorithm}:\n",
-        algorithm.best_model.solution,
+        f"THE BEST SCHEDULE FOUND WHEN USING {algorithm_name}:\n",
+        model.solution,
         "\n POINTS: ",
-        algorithm.best_model.calc_total_penalty(),
+        model.calc_total_penalty(),
         "\n evening points:",
-        algorithm.best_model.calc_evening_penalties(),
+        model.calc_evening_penalties(),
         "\n conflict points:",
-        algorithm.best_model.calc_student_schedule_penalties(),
+        model.calc_student_schedule_penalties(),
         "\n capacity penalty:",
-        algorithm.best_model.calc_total_capacity_penalties(),
+        model.calc_total_capacity_penalties(),
         "\n runtime:",
         runtime,
     )
