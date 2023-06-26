f576303ac68d920bd3e1f6b48bcc1b5b73ba5032
Nina van der Meulen <ninaajwzz@gmail.com>
2023-06-26 13:04:52 +0200

    updated

--- libraries/helpers/print_results.py
+++ libraries/helpers/print_results.py
@@ -5,19 +5,9 @@ def print_results(algorithm: str, model: Model, runtime):
     """
     Prints results of a model generated with an algorithm.
     """
-<<<<<<< HEAD
-    
-    print(f'THE BEST SCHEDULE FOUND WHEN USING {algorithm.upper()}:\n', model.solution,
-        '\n POINTS: ', model.total_penalty(),
-        '\n evening points:', model.evening_penalty(),
-        '\n conflict points:', model.student_schedule_penalties(),
-        '\n capacity penalty:', model.total_capacity_penalties(),
-        '\n runtime:', runtime
-    )
-=======
 
     print(
-        f"THE BEST SCHEDULE FOUND WHEN USING {algorithm.upper}:\n",
+        f"THE BEST SCHEDULE FOUND WHEN USING {algorithm.upper()}:\n",
         model.solution,
         "\n POINTS: ",
         model.calc_total_penalty(),
@@ -30,4 +20,3 @@ def print_results(algorithm: str, model: Model, runtime):
         "\n runtime:",
         runtime,
     )
->>>>>>> 4e3f60e (new baseline, removed bug)
