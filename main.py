9993e5d3d51a1b3f223b76aea0fb90e776600318
Nina van der Meulen <ninaajwzz@gmail.com>
2023-06-27 15:00:21 +0200

    command line args added

--- main.py
+++ main.py
@@ -10,77 +10,97 @@ from libraries.algorithms.simulated_annealing import SimulatedAnnealing
 from libraries.algorithms.random_restart import random_restart
 import random
 import time
-import random
+import argparse
 
 random.seed(0)
 
-if __name__ == "__main__":
+def main(algorithm, runs):
     empty_model = Model()
 
     # _________________________RANDOM ALGORITHM________________________________________
-    random_algorithm = Random(empty_model)
+    if algorithm == 'random':
+        random_algorithm = Random(empty_model)
 
-    # start_time = time.time()
-    random_algorithm.run(runs=2, verbose=True)
-    # runtime = start_time - time.time()
+        start_time = time.time()
+        random_algorithm.run(runs=runs, verbose=True)
+        runtime = start_time - time.time()
 
-    # print_results("random", random_algorithm.model, runtime)
+        print_results("random", random_algorithm.best_model, runtime)
 
     # ________________________BEAM SEARCH ALGORITHM____________________________________
+    elif algorithm == 'beam_search':
+        beam_search = BeamSearch(empty_model)
+        print("STARTING BEAM SEARCH ALGORITHM \n")
 
-    beam_search = BeamSearch(empty_model)
-    print("STARTING BEAM SEARCH ALGORITHM \n")
+        start_time = time.time()
+        beam_search.run(beam=1, runs=runs, heuristic="capacity", verbose=True)
+        runtime = start_time - time.time()
 
-    start_time = time.time()
-    beam_search.run(beam=1, runs=1, heuristic="capacity", verbose=True)
-    runtime = start_time - time.time()
+        visualize(beam_search.initial_model)
 
-    visualize(beam_search.initial_model)
-
-    # print_results("beam search", beam_search.initial_model, runtime)
+        print_results("beam search", beam_search.initial_model, runtime)
 
     # ______________________HILLCLIMBER ALGORITHM______________________________________
-    # hillclimber = HillClimber(random_algorithm.best_model)
-    # print("\n STARTING HILLCLIMBER ALGORITHM")
+    elif algorithm == 'hillclimber':
+        hillclimber = HillClimber(random_algorithm.best_model)
+        print("\n STARTING HILLCLIMBER ALGORITHM")
 
-    # start_time = time.time()
-    # hillclimber.run(verbose=True, heuristics=['middle', 'day'])
-    # runtime = time.time() - start_time
+        start_time = time.time()
+        hillclimber.run(verbose=True, heuristics=['middle', 'day'])
+        runtime = time.time() - start_time
 
-    # print_results('hillclimber', hillclimber.best_model, runtime)
+        print_results('hillclimber', hillclimber.best_model, runtime)
 
     # ______________________SIMULATED ANNEALING________________________________________
-    simulated_annealing = SimulatedAnnealing(random_algorithm.best_model, temperature=10)
+    elif algorithm == 'simulated_annealing':
+        simulated_annealing = SimulatedAnnealing(random_algorithm.best_model, temperature=10)
 
-    random_restart(SimulatedAnnealing, heuristics=['middle', 'day'], verbose=True, iterations=200)
-    start_time = time.time()
-    simulated_annealing.run(iterations=100, verbose=True, heuristics=['middle', 'day'], type="exponential", alpha=0.95)
-    runtime = time.time() - start_time
+        random_restart(SimulatedAnnealing, heuristics=['middle', 'day'], verbose=True, iterations=200)
+        start_time = time.time()
+        simulated_annealing.run(iterations=100, verbose=True, heuristics=['middle', 'day'], type="exponential", alpha=0.95)
+        runtime = time.time() - start_time
 
-    print_results('simulated annealing', simulated_annealing.best_model, runtime)
+        print_results('simulated annealing', simulated_annealing.best_model, runtime)
 
     # ________________________GREEDY ALGORITHM_________________________________________
-    # start_time = time.time()
-    # greedy_solution = Greedy(empty_model).run()
-    # runtime = time.time() - start_time
+    elif algorithm == 'greedy':
+        start_time = time.time()
+        greedy_solution = Greedy(empty_model).run()
+        runtime = time.time() - start_time
 
-    # print_results('greedy', greedy_solution, runtime)
+        print_results('greedy', greedy_solution, runtime)
 
     # ________________________RANDOMGREEDY ALGORITHM___________________________________
-    # start_time = time.time()
-    # random_greedy = RandomGreedy(empty_model).run()
-    # runtime = time.time() - start_time
+    elif algorithm == 'random_greedy':
+        start_time = time.time()
+        random_greedy = RandomGreedy(empty_model).run()
+        runtime = time.time() - start_time
 
-    # print_results('randomgreedy', random_greedy, runtime)
+        print_results('randomgreedy', random_greedy, runtime)
 
     # __________________________BASELINE_______________________________________________
-    # random.seed(0)
-    # with open("results/baseline.txt", "a+") as file:
-    #     for i in range(10000):
-    #         penalty = []
-    #         for j in range(100):
-    #             random_schedule = Random(empty_model)
-    #             penalty.append(random_schedule.run().calc_total_penalty())
-    #             print(f"Current run: {i * 100 + j + 1}", end="\r")
-    #         text = "\n".join([str(score) for score in penalty])
-    #         file.write(f"\n{text}")
+    elif algorithm == 'baseline':
+        random.seed(0)
+        with open("results/baseline.txt", "a+") as file:
+            for i in range(10000):
+                penalty = []
+                for j in range(100):
+                    random_schedule = Random(empty_model)
+                    penalty.append(random_schedule.run().calc_total_penalty())
+                    print(f"Current run: {i * 100 + j + 1}", end="\r")
+                text = "\n".join([str(score) for score in penalty])
+                file.write(f"\n{text}")
+
+if __name__ == "__main__":
+    # set-up parsing command line arguments
+    parser = argparse.ArgumentParser(description = "run a specific algorithm")
+
+    # adding arguments
+    parser.add_argument("a", help = "algorithm to run")
+    parser.add_argument("--n", help = "number of runs", default=1, type=int)
+
+    # read arguments from command line
+    args = parser.parse_args()
+
+    # run main with provided arguments
+    main(args.a, args.n)
